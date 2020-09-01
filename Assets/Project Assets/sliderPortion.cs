9990523682b821c9edc086e4c34d63bf8edf38e5
Damian <bembendamian@yahoo.co.uk>
2020-09-01 21:12:27 +0100

    some further updates

--- Assets/Project Assets/sliderPortion.cs
+++ Assets/Project Assets/sliderPortion.cs
@@ -16,9 +16,11 @@ public class sliderPortion : MonoBehaviour
     public GameObject[] objectsToShow;
     public GameObject submitButton;
     public InteractionSlider leapSlider;
-    public UnityEngine.UI.Text cigaretteAmount;
-    public UnityEngine.UI.Text chosenValue;
-    public UnityEngine.UI.Text actualValue;
+    public TextMesh cigaretteAmount;
+    public TextMesh mortalityReduction;
+    public TextMesh chosenValue;
+    public TextMesh actualValue;
+    public TextMesh actualReduction;
     bool submit = false;
     InteractionButton interactionBehaviour1;
 
@@ -39,21 +41,40 @@ public class sliderPortion : MonoBehaviour
 
         float sliderVal = leapSlider.HorizontalSliderValue;
         float cigarettes = CalculateCigarettes(sliderVal);
-        cigaretteAmount.text = cigarettes.ToString();
+        cigaretteAmount.text = cigarettes.ToString() + " Cigarettes Per Week";
+        mortalityReduction.text = lifeSpan(sliderVal, sliderVal) + "Months that could be gained if adhering to WHO standards";
         if (submit)
         {
             foreach (GameObject objecttoHide in objectsToHide)
             {
                 objecttoHide.SetActive(false);
             }
+            chosenValue.text = cigarettes.ToString() + " Cigarettes Per Week";
+            actualValue.text = CalculateCigarettes(pm2) + " Cigarettes Per Week";
+            actualReduction.text = lifeSpan(pm10, pm2) + "Months that could be gained if adhering to WHO standards";
             foreach (GameObject objecttoshow in objectsToShow)
             {
                 objecttoshow.SetActive(true);
             }
             submit = false;
+
         }
     }
-
+    float lifeSpan(float PM10, float PM2)
+    {
+        float validPM10 = 20f;
+        float validPM2 = 12f;
+        float PM10inc = (PM10 - validPM10) / 10.0f;
+        float PM2inc = (PM2 - validPM2) / 10.0f;
+        if (PM10inc < 0.0f && PM2inc < 0.0f)
+        {
+            return (0.0f);
+        }
+        else
+        {
+            return ((PM10inc * 7.0f) + (PM2inc * 12.0f));
+        }
+    }
     float CalculateCigarettes(float pm2val)
     {
         float base_level = 22.0f;
